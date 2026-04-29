89fb82a3fad56d4a8210e941bfc7ae1fa8129173
Yoshitha <yoshithapathikonda09@gmail.com>
2026-04-29 17:52:51 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,1 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.SigUp , name='userlogin') ,
-    path('UserLogin/' , views.UserLogin , name='UserLogin'),
-    path('User_Home/' , views.UserHome , name='UserHome'),
-    path('Traning/' , views.Traning , name='Traning'),
-    path('predict/' , views.predict , name='predict')
 
-]
